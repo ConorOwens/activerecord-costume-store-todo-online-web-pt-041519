9e866e93bd763bfce8d6d868bece9866139053b5
Conor Owens <conorj.owens@gmail.com>
2019-07-27 01:45:04 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,7 @@
 
 class CreateHauntedHouses  < ActiveRecord::Migration[4.2]
   def change
-    create_table :costumes do |a|
+    create_table :haunted_houses do |a|
       a.string :name 
       a.string :location
       a.string :theme
