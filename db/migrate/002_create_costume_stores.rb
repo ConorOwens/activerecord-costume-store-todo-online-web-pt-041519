94ab4ba402309de89f1a7bdf50876c05d4de779a
Conor Owens <conorj.owens@gmail.com>
2019-07-27 01:33:02 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,12 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores  < ActiveRecord::Migration
+  def change
+    create_table :costumes do |a|
+      a.string :name 
+      a.float :price
+      a.string :url
+      a.string :size 
+      a.timestamps
+    end
+  end
